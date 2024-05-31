e64db8fccc1e531f2ce0f71dee26d48898e339e5
Marshall FRY <marshalfry1998@gmail.com>
2024-05-31 22:42:34 +0200

    add description page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { AtomesModule } from 'library_v2/src/app/components/atomes/atomes.module';
+import { DescriptionComponent } from './description/description.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomePageComponent
+    WelcomePageComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule,
