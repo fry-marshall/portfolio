bf5cf44004265f654e0327cc3ac8d6675dc787e4
Marshall FRY <marshalfry1998@gmail.com>
2024-06-09 16:43:46 +0200

    add horizontal scroll

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -30,6 +30,37 @@ export class ProjectsComponent implements AfterViewInit {
       delay: .2,
       duration: .1
     })
+
+    const content = document.querySelector(".content-project");
+
+    const tween = gsap.to(content, {
+      x: this.getScrollAmount,
+      duration: 3,
+      ease: "none",
+      onStart: () => {
+        console.log("yeah")
+        /* document.body.style.position = 'fixed'
+        document.body.style.width = '100%' */
+      }
+    });
+    
+    
+    ScrollTrigger.create({
+      trigger:".container-project",
+      start:"top 20%",
+      end: () => `+=${this.getScrollAmount() * -1}`,
+      animation:tween,
+      scrub:1,
+      invalidateOnRefresh:true,
+      markers:true
+    })
+
+  }
+
+  getScrollAmount() {
+    const content = document.querySelector(".content-project");
+    let contentWidth = content!.scrollWidth;
+    return -(contentWidth - window.innerWidth);
   }
 
 }
